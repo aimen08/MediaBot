7380387362665928811261bf34963d1798548309
aimen08 <aimen_hamza@hotmail.com>
2023-02-21 15:29:38 +0100

    fixed insta v3

--- scrapers/instagramScraper.py
+++ scrapers/instagramScraper.py
@@ -1,7 +1,8 @@
 import requests
 import json
 import cloudscraper
-from loguru import logger
+from bs4 import BeautifulSoup
+
 
 scraper = cloudscraper.create_scraper() 
 
@@ -9,22 +10,25 @@ def getInstagramVideo(url):
 
     try:   
         headers= {
-            'url':url,
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
-            'origin': 'https://instavideosave.net',
+            'origin':'https://instaoffline.net',
+            'referer':'https://instaoffline.net/video-downloader/',
+            'content-type':'application/x-www-form-urlencoded; charset=UTF-8',
             'accept': '*/*'
         }
-
-        response = scraper.get('https://api.instavideosave.com/allinone', headers=headers)
-        logger.info(response.text)
-        link = json.loads(response.text)["video"][0]["video"]
+        data = {
+            'q': url,
+            'downloader': 'video'
+        }
+        response = scraper.post('https://instaoffline.net/process/',data=data, headers=headers)
+       
+        soup = BeautifulSoup(json.loads(response.text)['html'], 'html.parser')    
+        link = soup.findAll('a',attrs={'class':'button'})[0]['href']
         return link
 
     except Exception as e:
-        logger.info(e)
         return {
             'success': False,
             'error': e
         }
         
-
