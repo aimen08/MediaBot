57b255fc343986796e69970d998fa2b28e36a52e
aimen08 <aimen_hamza@hotmail.com>
2023-02-21 16:01:00 +0100

    fix insta v5

--- scrapers/instagramScraper.py
+++ scrapers/instagramScraper.py
@@ -11,19 +11,18 @@ def getInstagramVideo(url):
     try:   
         headers= {
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
-            'origin':'https://instaoffline.net',
-            'referer':'https://instaoffline.net/video-downloader/',
+            'origin':'https://snapinsta.io',
+            'referer':'https://snapinsta.io/en2/instagram-downloader',
             'content-type':'application/x-www-form-urlencoded; charset=UTF-8',
             'accept': '*/*'
         }
         data = {
             'q': url,
-            'downloader': 'video'
+            'vt': 'facebook'
         }
-        response = scraper.post('https://instaoffline.net/process/',data=data, headers=headers)
-        logger.info(response.text)
-        soup = BeautifulSoup(json.loads(response.text)['html'], 'html.parser')    
-        link = soup.findAll('a',attrs={'class':'button'})[0]['href']
+        response = scraper.post('https://snapinsta.io/api/ajaxSearch/instagram',data=data, headers=headers)  
+        soup = BeautifulSoup(json.loads(response.text)['data'], 'html.parser')   
+        link = soup.findAll('a',attrs={'class':'abutton is-success is-fullwidth btn-premium mt-3'})[0]['href']
         return link
 
     except Exception as e:
